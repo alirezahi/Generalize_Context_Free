9726797a23aadba8b888dc7c56fff4d79501ab1b
Alireza Heydari <alireza97hi@gmail.com>
2018-05-22 13:12:07 +0430

    remove lambda added

---
+++ main.py
@@ -0,0 +1,71 @@
+import string
+from itertools import chain, combinations
+
+# Alphabet of our language
+alphabet = list(string.ascii_lowercase)
+# Variables used in our language
+variables = list(string.ascii_uppercase)
+
+
+def powerset(iterable):
+    """
+    powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+    """
+    xs = list(iterable)
+    # note we return an iterator rather than a list
+    return chain.from_iterable(combinations(xs, n) for n in range(len(xs) + 1))
+
+class ContextFree():
+    def __init__(self,r,s):
+        self.v = variables
+        self.sigma = alphabet
+        self.r = r
+        self.s = s
+
+    def generalize(self):
+        self.__remove_lambda()
+        return 
+
+    def __remove_lambda(self):
+        v_n = []
+        for leading_rule,result_rule in self.r.items():
+            for right_result in result_rule:
+                print(leading_rule)
+                print(right_result)
+                print()
+                if right_result == 'λ':
+                    v_n.append(leading_rule)
+        sth_new_added = True
+        while sth_new_added:
+            sth_new_added = False
+            for leading_rule,result_rule in self.r.items():
+                if leading_rule not in v_n:
+                    need_to_be_vn = False
+                    for right_result in result_rule:
+                        need_to_be_vn = False
+                        for element in right_result:
+                            if not(element in variables):
+                                need_to_be_vn = True
+                    if need_to_be_vn:
+                        sth_new_added = True
+                        v_n.append(leading_rule)
+        new_grammer = {}
+        for leading_rule, result_rule in self.r.items():
+            for right_result in result_rule:
+                for combination in powerset(v_n):
+                    if len(combination) > 0:
+                        for element_of_combination in combination:
+                            new_result = right_result.replace(element_of_combination,'')
+                            new_result = new_result.replace('λ', '')
+                        if len(new_result) > 0:
+                            if leading_rule not in new_grammer:
+                                new_grammer[leading_rule] = []
+                            new_grammer[leading_rule].append(new_result)
+        for leading,result in new_grammer.items():
+            new_grammer[leading] = list(set(result))
+        self.r = new_grammer
+
+
+
+c = ContextFree({'S': ['λ', 'Ab'], 'A': ['λ','a']},'a')
+c.generalize()
